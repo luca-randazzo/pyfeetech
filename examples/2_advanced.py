faaaebb251fecedce4850f3b9c2a736c5e4d521c
LR <luca.randazzo@epfl.ch>
2025-08-16 13:40:52 +0200

    added comments to code

--- examples/2_advanced.py
+++ examples/2_advanced.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# file allows to test pyfeetech library
+# this file allows to test pyfeetech library
 # advanced functionalities
 
 # -----------------------------------------------
@@ -11,7 +11,6 @@ import os
 import time
 import threading
 # imports for keyboard
-#import select
 import tty
 import termios
 import atexit
@@ -33,16 +32,16 @@ else:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
-# Uses sts-servo_sdk library
-from pyfeetech import * # pyfeetech library
+# pyfeetech library
+from pyfeetech import * 
 
 # ----------------------------------------------------------------------------------------------
 # variables
 # ----------------------------------------------------------------------------------------------
 # Default setting
-STS_ID          = 3                 # ID
+STS_ID          = 4                 # ID
 STS_BAUDRATE    = 1000000           # default baudrate : 1000000
-STS_PORT        = '/dev/ttyACM0'    # e.g.: Windows: "COM1", Linux: "/dev/ttyUSB0"
+STS_PORT        = '/dev/ttyACM0'    # e.g.: Windows: "COM1", Linux: "/dev/ttyACM0"
 
 thread_sleep_time = 0.001
 
@@ -133,54 +132,64 @@ if __name__ == "__main__":
 
 
     # --------- Control motors directly via bus
+    # for all eeprom calls, if you want changes to be stored on the motor also after power-cycling
+    # you have to: set lock to 0, then write on the register, and then set lock to 1
+    # if the lock is not set to 0, changes will not be stored on the motor after power-cycling,
+    # i.e. they will only be applied to the current session
+
     # - set ID
-    #bus.set_lock(1, 0)  # motor with ID=1: set lock status = 0. this ensures that the next EEPROM write is stored on the motor also after power-cycling it
-    #bus.set_id(1, 7)    # motor with ID=1: set ID=7
-    #bus.set_lock(1, 1)  # motor with ID=1: set lock status = 1
-
-    # - activate/deactivate torque
-    #bus.set_torque(STS_ID, 0)
-
-    # - set speed and acceleration
-    #bus.set_speed(STS_ID, 500)            # 3400 is the max speed according to the Feetech control table
-    #bus.set_acceleration(STS_ID, 50)
+    #bus.eeprom_set_lock(1, 0)     # motor with ID = 1: set lock status = 0. this ensures that the next EEPROM write is stored on the motor also after power-cycling it
+    #bus.eeprom_set_id(1, 7)       # motor with ID = 1: set ID=7
+    #bus.eeprom_set_lock(1, 1)     # motor with ID = 1: set lock status = 1
 
     # - set max torque
-    #bus.set_torque_max(STS_ID, 100)
+    #bus.eeprom_set_lock(STS_ID, 0)
+    #bus.eeprom_set_torque_max(STS_ID, 1000)
+    #bus.eeprom_set_lock(STS_ID, 1)
     #print(f"[__main__] torque max:         {bus.get_torque_max(STS_ID):04d}")
 
     # - set min startup force
-    #bus.set_force_startup_min(STS_ID, 500)
+    #bus.eeprom_set_lock(STS_ID, 0)
+    #bus.eeprom_set_force_startup_min(STS_ID, 1000)
+    #bus.eeprom_set_lock(STS_ID, 1)
     #print(f"[__main__] force_startup_min:  {bus.get_force_startup_min(STS_ID):04d}")
 
+    # - activate/deactivate torque
+    #bus.sram_set_torque_enable(STS_ID, 0)
+    #print(f"[__main__] torque enable:         {bus.get_torque_enable(STS_ID):04d}")
+
+    # - set speed and acceleration
+    bus.sram_set_speed(STS_ID, 254)
+    bus.sram_set_acceleration(STS_ID, 254)
+
     # - set torque limit
-    #bus.set_torque_limit(STS_ID, 500)
+    #bus.sram_set_torque_limit(STS_ID, 500)
     #print(f"[__main__] torque limit:       {bus.get_torque_limit(STS_ID):04d}")
 
     # - position mode
-    #bus.set_mode(STS_ID, STS_MODE_POSITION)
-    #bus.set_position(STS_ID, 0)
+    #bus.eeprom_set_mode(STS_ID, STS_MODE_POSITION)
+    #bus.sram_set_position(STS_ID, 0)
 
     # - step mode
-    #bus.set_mode(STS_ID, STS_MODE_STEP)
-    #bus.set_position(STS_ID, 100)  # +100 steps
-    #bus.set_position(STS_ID, -100) # -100 steps
+    #bus.eeprom_set_mode(STS_ID, STS_MODE_STEP)
+    #bus.sram_set_position(STS_ID, 100)  # +100 steps
+    #bus.sram_set_position(STS_ID, -100) # -100 steps
 
     # - multiturn mode
     # note: Feetech manual reports "power failure does not save the number of turns"
     #       i.e. if motor is powered off, it will not record how many muliturns it made,
     #       but it will start from the current value of its encoder
     # in order to activate multiturn mode, the following 3 calls have to be made 
-    bus.set_mode(STS_ID, STS_MODE_POSITION)    
-    bus.set_angle_min(STS_ID, 0)
-    bus.set_angle_max(STS_ID, 0)
+    #bus.eeprom_set_mode(STS_ID, STS_MODE_POSITION)    
+    #bus.eeprom_set_angle_min(STS_ID, 0)
+    #bus.eeprom_set_angle_max(STS_ID, 0)
     # then, motor can achieve multiturns with e.g.:
-    #bus.set_position(STS_ID, -1*4095)  # -1 full rotation
-    #bus.set_position(STS_ID, 1*4095)   # +1 full rotation
+    #bus.sram_set_position(STS_ID, -1*4095)  # -1 full rotation
+    #bus.sram_set_position(STS_ID, 1*4095)   # +1 full rotation
 
     # - speed mode
-    #bus.set_mode(STS_ID, STS_MODE_SPEED)
-    #bus.set_speed(STS_ID, 500)
+    #bus.eeprom_set_mode(STS_ID, STS_MODE_SPEED)
+    #bus.sram_set_speed(STS_ID, 500)
 
     # - test print_status() function
     #bus.print_status(0b11111)    
@@ -214,17 +223,17 @@ if __name__ == "__main__":
 
             # --- Process keyboard
             if      key==1: # arrow up
-                bus.set_position(STS_ID, -2*4095) # multiturn mode
+                bus.sram_set_position(STS_ID, -1*4095) # multiturn mode
                 #bus.set_position(STS_ID, 100)  # +100 steps
                 key = 0
             
             elif    key==2: # arrow down
-                bus.set_position(STS_ID, 2*4095)  # multiturn mode
+                bus.sram_set_position(STS_ID, 1*4095)  # multiturn mode
                 #bus.set_position(STS_ID, -100)  # -100 steps
                 key = 0
             
             elif    key==3: # spacebar
-                bus.set_position(STS_ID, 0)
+                bus.sram_set_position(STS_ID, 0)
                 key = 0
 
     except KeyboardInterrupt:
@@ -232,7 +241,7 @@ if __name__ == "__main__":
 
     finally:
         # - deactivate torque
-        bus.set_torque(STS_ID, 0)
+        bus.sram_set_torque_enable(STS_ID, 0)
         
         # - close connection
         portHandler.closePort()
